b038f7d0c2d511ce0dab0a788569cc9cf4a2631e
yassshuuu <102218298+yassshuuu@users.noreply.github.com>
2024-07-29 22:36:08 +0530

    Create PrintNoWithWhile.java

---
+++ Day3/PrintNoWithWhile.java
@@ -0,0 +1,12 @@
+package Day3;
+
+public class PrintNoWithWhile {
+        public static void main(String[] args) {
+            int i = 1;
+            while (i <= 10) {
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+    
